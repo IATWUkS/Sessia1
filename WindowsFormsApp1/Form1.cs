65eb713e4faf1551684f5240183cb9e425ffcf1b
unknown <IATWUKS>
2021-04-21 22:24:23 +0300

    First project

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string ConnString = "server=127.0.0.1,3306;user id=root;database=nmn";
+            SqlConnection connect = new SqlConnection(ConnString);
+            SqlCommand command = new SqlCommand("SELECT name FROM test", connect);
+        }
+    }
+}
